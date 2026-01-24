08c32889c817d4a716e98eb8efa2fb90d69421b6
Ahmed Fahim <82629098+A7med31fimo@users.noreply.github.com>
2026-01-24 16:37:10 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dotnet-roadmap-tracker/'  // Replace with your repo name
+  base: '/roadmap-tracker/'  // Replace with your repo name
 })
